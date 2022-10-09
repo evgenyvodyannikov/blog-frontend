a348cd0c3313ed5d5a1d2449b40f9cf09d17dafb
evgenyvodyannikov <66135471+evgenyvodyannikov@users.noreply.github.com>
2022-10-09 23:10:11 +0500

    add main folders and files

---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+import styles from './header.module.scss';
+import Container from '@mui/material/Container';
+
+export const Header = () => {
+
+    return(
+        <>
+        
+        </>
+    )
+}
