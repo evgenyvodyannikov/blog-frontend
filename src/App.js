ee08b147696fcb5f237d684798162be9aeebbfae
eugenevodyannikov <vodyannikov.eugene@gmail.com>
2022-10-11 11:16:28 +0500

    create header layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import Container from '@mui/material/Container';
 import { Header } from './components'
 
 function App() {
   return (
     <>
     <Header/>
+    <Container maxWidth='lg'>
+      <></>
+    </Container>
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 
-import styles from './header.module.scss';
+import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 
 export const Header = () => {
 
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <a className={styles.logo} href="/">
+            <div>blogName</div>
+          </a>
+          <div className={styles.buttons}>
+              <>
+                <a href="/login">
+                  <Button variant="outlined">Войти</Button>
+                </a>
+                <a href="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </a>
+              </>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
