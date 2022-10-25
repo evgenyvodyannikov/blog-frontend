f8704f8dd3bfd3ccfbf8140eb78a25528791a34f
Evgeny Vodyannikov <vodyannikov.eugene@gmail.com>
2022-10-25 10:40:21 +0500

    add post slice

---
+++ src/redux/slices/posts.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    posts: {
+        items: [],
+        status: 'loading',
+    },
+    tags: {
+        items: [],
+        status: 'loading',
+    },
+    comments: {
+        items: [],
+        status: 'loading',
+    },
+};
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducer: {},
+});
+
+export const postsReducer = postsSlice.reducer;
