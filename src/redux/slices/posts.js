6d155b64049dbe399a03546a4ce9b71941539071
Evgeny Vodyannikov <vodyannikov.eugene@gmail.com>
2022-10-25 11:21:09 +0500

    add request status tracking

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -25,6 +25,20 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducer: {},
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.posts.items = action.payload;
+            state.posts.status = 'loaded';
+        },
+        [fetchPosts.rejected]: (state, action) => {
+            state.posts.items = [];
+            state.posts.status = 'error';
+        },
+    }
 });
 
 export const postsReducer = postsSlice.reducer;
