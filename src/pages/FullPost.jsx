5753973a03544e9038accb2c5c8e13275090c808
Evgeny Vodyannikov <vodyannikov.eugene@gmail.com>
2022-10-26 11:30:44 +0500

    add receiving data for open post

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,33 +1,46 @@
 import React from "react";
+import { useParams } from 'react-router-dom'
 
 import { Post } from "../components/Post";
 import { Index } from "../components/LeaveComment";
 import { CommentsBlock } from "../components/CommentsBlock";
+import axios from "../axios";
 
 export const FullPost = () => {
+  const [data, setData] = React.useState();
+  const [isLoading, setLoading] = React.useState(true);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    axios.get(`/posts/${id}`).then(res => {
+      setData(res.data);
+      setLoading(false);
+    }).catch(err => {
+      console.warn(err);
+      alert('Ошибка получения статьи');
+    })
+  }, []);
+
+  if (isLoading) {
+    return (<Post isLoading={true} isFullPost />);
+  }
+
+
   return (
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
+        _id={data._id}
+        title={data.title}
         imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
-        }}
-        createdAt={"12 июня 2022 г."}
-        viewsCount={150}
-        commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
+        commentsCount={data.commentsCount}
+        tags={data.tags}
         isFullPost
       >
         <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
+          {data.text}
         </p>
       </Post>
       <CommentsBlock
@@ -37,7 +50,7 @@ export const FullPost = () => {
               fullName: "Вася Пупкин",
               avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
             },
-            text: "Это тестовый комментарий 555555",
+            text: "дадаада афщы шаф тащфы щоафщ ытфы тща щфтшыащ тф ащфыт атщф ыщатш фыщ",
           },
           {
             user: {
