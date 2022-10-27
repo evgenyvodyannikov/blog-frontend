8119d2286a031932352ff0a3cedec7cf4c7dac87
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-10-27 23:01:36 +0500

    add auth request

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,26 +3,55 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { fetchUserData } from '../../redux/slices/auth';
 
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+
+  const dispatch = useDispatch();
+
+  const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange'
+  });
+
+  const onSubmit = (values) => {
+    dispatch(fetchUserData(values));
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error={Boolean (errors.email?.message)}
+          helperText={ errors.email?.message }
+          {...register('email', { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          error={Boolean (errors.password?.password)}
+          helperText={ errors.password?.password }
+          {...register('password', { required: "Укажите пароль" })}
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form >
     </Paper>
+
   );
 };
