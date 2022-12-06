fc2078c714f539c5cc39a57c251375da34db530a
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-12-06 19:46:09 +0500

    submit creating post and navigate to it

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -9,23 +9,66 @@ import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import { selectIsAuth } from '../../redux/slices/auth';
 
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import axios from '../../axios';
 
 
 export const AddPost = () => {
-  const isAuth = useSelector(selectIsAuth);
 
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
+  const isAuth = useSelector(selectIsAuth);
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = React.useState(false);
+  const [text, setText] = React.useState('');
   const [title, setTitle] = React.useState('');
   const [tags, setTags] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
 
-  const handleChangeFile = () => { };
+  const inputFileRef = React.useRef(null);
 
-  const onClickRemoveImage = () => { };
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    }
+    catch (error) {
+      console.warn(error);
+    }
+  };
 
-  const onChange = React.useCallback((value) => {
-    setValue(value);
+  const onSubmit = async () => {
+
+    try{
+      setLoading(true);
+
+      const fields = {
+        title,
+        imageUrl,
+        tags,
+        text
+      }
+
+      const { data } = await axios.post('/posts', fields);
+      
+      const id = data._id;
+
+      navigate(`/posts/${id}`);
+    }
+    catch(error){
+      console.warn(error);
+      alert('Ошибка при создании статьи');
+    }
+  }
+
+  const onClickRemoveImage = () => { 
+    setImageUrl('');
+  };
+
+  const onChange = React.useCallback((text) => {
+    setText(text);
   }, []);
 
   const options = React.useMemo(
@@ -49,17 +92,17 @@ export const AddPost = () => {
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Удалить
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        </>
       )}
       <br />
       <br />
@@ -78,9 +121,9 @@ export const AddPost = () => {
         value={tags}
         onChange={(e) => setTags(e.target.value)}
         fullWidth />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
+        <Button onClick={onSubmit} size="large" variant="contained">
           Опубликовать
         </Button>
         <Link to="/">
