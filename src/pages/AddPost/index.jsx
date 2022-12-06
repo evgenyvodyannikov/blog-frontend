4e29d72cbaf741a6b63d32ce5b47c868b1370289
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-12-06 18:35:59 +0500

    navigate to home page if non authorized user tries add post

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -3,13 +3,18 @@ import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import SimpleMDE from 'react-simplemde-editor';
+import { useSelector } from 'react-redux';
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import { selectIsAuth } from '../../redux/slices/auth';
 
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+
 
 export const AddPost = () => {
+  const isAuth = useSelector(selectIsAuth);
+
   const imageUrl = '';
   const [value, setValue] = React.useState('');
 
@@ -35,6 +40,10 @@ export const AddPost = () => {
     }),
     [],
   );
+
+  if(!isAuth){
+    return <Navigate to="/" />;
+  }
 
   return (
     <Paper style={{ padding: 30 }}>
